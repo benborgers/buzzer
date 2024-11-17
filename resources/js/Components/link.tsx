02a06b640e7e6bacd5e74de3cfe4d8263657ef76
Ben Borgers <borgersbenjamin@gmail.com>
2024-11-17 10:53:11 -0500

    Make Catalyst Link into Inertia's Link

--- resources/js/Components/link.tsx
+++ resources/js/Components/link.tsx
@@ -1,21 +1,14 @@
-/**
- * TODO: Update this component to use your client-side framework's link
- * component. We've provided examples of how to do this for Next.js, Remix, and
- * Inertia.js in the Catalyst documentation:
- *
- * https://catalyst.tailwindui.com/docs#client-side-router-integration
- */
-
-import * as Headless from '@headlessui/react'
-import React, { forwardRef } from 'react'
+import * as Headless from '@headlessui/react';
+import { Link as InertiaLink, type InertiaLinkProps } from '@inertiajs/react';
+import React, { forwardRef } from 'react';
 
 export const Link = forwardRef(function Link(
-  props: { href: string } & React.ComponentPropsWithoutRef<'a'>,
-  ref: React.ForwardedRef<HTMLAnchorElement>
+  props: InertiaLinkProps,
+  ref: React.ForwardedRef<HTMLAnchorElement>,
 ) {
   return (
     <Headless.DataInteractive>
-      <a {...props} ref={ref} />
+      <InertiaLink {...props} ref={ref} />
     </Headless.DataInteractive>
-  )
-})
+  );
+});
